8fd332c983bc29583e5c1bd0de1ae349385d1736
m55 <m55@localhost.localdomain>
2017-10-01 22:38:35 +0300

    create-operator fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -78,6 +78,9 @@ class LoginController extends Controller
                         {
                             return redirect()->route('dashboard.show-dashboard.get');
                         }
+                } else {
+                    Session::flash('error', 'Ошибка авторизации');
+                    return redirect()->back();
                 }
             } else {
                 /**

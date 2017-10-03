ee4fd039069dd0a3acf3c14a436631888954e7ed
m55 <m55@localhost.localdomain>
2017-10-03 04:23:20 +0300

    new profile page

--- resources/views/dashboard/partner_page.blade.php
+++ resources/views/dashboard/partner_page.blade.php
@@ -16,6 +16,102 @@
             <div class="container-fluid">
                 @include('includes/notifications')
                 <div class="row">
+                    <div class="col-lg-4 col-md-5">
+                            <div class="card card-user">
+                                <div class="image">
+                                    <img src="{{ $partner->thumbnail }}">
+                                </div>
+                                <div class="card-content">
+                                    <div class="author">
+                                      <img class="avatar border-white" src="{{ $partner->logo }}">
+                                      <h4 class="card-title">{{ $partner->name }}<br>
+                                         <a><small>{{ $partner->fullname }}</small></a>
+                                      </h4>
+                                    </div>
+                                    <p class="description text-center">
+                                        "I like the way you work it <br>
+                                        No diggity <br>
+                                        I wanna bag it up"
+                                    </p>
+                                </div>
+                                <hr>
+                                <div class="text-center">
+                                    <div class="row">
+                                        <div class="col-md-3 col-md-offset-1">
+                                            <h5>{{ $balance->value }}<br><small><i class="fa fa-money"></i> Баланс</small></h5>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <h5>2GB<br><small>Used</small></h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <h5>24,6$<br><small>Spent</small></h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Team Members</h4>
+                                </div>
+                                <div class="card-content">
+                                    <ul class="list-unstyled team-members">
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-xs-3">
+                                                    <div class="avatar">
+                                                        <img src="../../assets/img/faces/face-0.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                                    </div>
+                                                </div>
+                                                <div class="col-xs-6">
+                                                    DJ Khaled
+                                                    <br>
+                                                    <span class="text-muted"><small>Offline</small></span>
+                                                </div>
+                                                <div class="col-xs-3 text-right">
+                                                    <btn class="btn btn-sm btn-success btn-icon"><i class="fa fa-envelope"></i></btn>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-xs-3">
+                                                    <div class="avatar">
+                                                        <img src="../../assets/img/faces/face-1.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                                    </div>
+                                                </div>
+                                                <div class="col-xs-6">
+                                                    Creative Tim
+                                                    <br>
+                                                    <span class="text-success"><small>Available</small></span>
+                                                </div>
+                                                <div class="col-xs-3 text-right">
+                                                    <btn class="btn btn-sm btn-success btn-icon"><i class="fa fa-envelope"></i></btn>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-xs-3">
+                                                    <div class="avatar">
+                                                        <img src="../../assets/img/faces/face-3.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                                    </div>
+                                                </div>
+                                                <div class="col-xs-6">
+                                                    Flume
+                                                    <br>
+                                                    <span class="text-danger"><small>Busy</small></span>
+                                                </div>
+                                                <div class="col-xs-3 text-right">
+                                                    <btn class="btn btn-sm btn-success btn-icon"><i class="fa fa-envelope"></i></btn>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="row">
                     <div class="col-md-8">
                         <div class="row">
                             <div class="col-md-12">

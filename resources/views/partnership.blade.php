52d09d54c576d2796eb8243c8d2bbd630c62c75d
m55 <m55@localhost.localdomain>
2017-11-07 06:01:51 +0300

    tariffs at partnership page

--- resources/views/partnership.blade.php
+++ resources/views/partnership.blade.php
@@ -162,7 +162,75 @@
         </div>
     </div>
 
+<div class="section-white">
+    <div class="pricing-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 ml-auto mr-auto text-center">
+                        <h2 class="title">Pick the best plan for you</h2>
+                        <h5 class="description">You have Free Unlimited Updates and Premium Support on each package.</h5>
+                        <br>
 
+                    </div>
+                </div>
+                <div class="space-top"></div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-pricing" data-background="image" style="background-image: url('assets/img/pages/partnership/price1.jpg')">
+                            <div class="card-body">
+                                <h6 class="card-category white-text">Малый бизнес</h6>
+                                <h1 class="card-title">Старт</h1>
+                                <ul>
+                                    <li class="white-text"><b>5</b> Sharing Tools</li>
+                                    <li class="white-text"><b>10</b> Design Tools</li>
+                                    <li class="white-text"><b>100</b> Private Messages</li>
+                                    <li class="white-text"><b>2</b> Personal Brand</li>
+                                </ul>
+                                <a href="#pablo" class="btn btn-warning btn-round ">
+                                    Get Started
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card card-pricing" data-background="image" style="background-image: url('assets/img/sections/forest-bg.jpg')">
+                            <div class="card-body">
+                                <h6 class="card-category">Premium</h6>
+                                <h1 class="card-title"><small>$</small>89<small>/mo</small></h1>
+                                <ul>
+                                    <li><b>100</b> Projects</li>
+                                    <li><b>5</b> Team Members</li>
+                                    <li><b>55</b> Personal Contacts</li>
+                                    <li><b>5.000</b> Messages</li>
+                                </ul>
+                                <a href="#pablo" class="btn btn-success btn-round">
+                                    Get Started
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card card-pricing" data-background="image" style="background-image: url('assets/img/sections/anders-jilden.jpg')">
+                            <div class="card-body">
+                                <h6 class="card-category">Platinum</h6>
+                                <h1 class="card-title"><small>$</small>199<small>/mo</small></h1>
+                                <ul>
+                                    <li><b>1</b> Project</li>
+                                    <li><b>5</b> Team Members</li>
+                                    <li><b>55</b> Personal Contacts</li>
+                                    <li><b>5.000</b> Messages</li>
+                                </ul>
+                                <a href="#pablo" class="btn btn-danger btn-round ">Get Started</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
 </div>
 </div>
 @endsection

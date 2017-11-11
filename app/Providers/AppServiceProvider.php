03813aeb362a47c7a5e18322482f48567bfedcd6
m55 <m55@localhost.localdomain>
2017-11-12 01:47:16 +0300

    TLS certificate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         $categories = DB::table('ETKPLUS_PARTNER_CATEGORIES')
                         ->orderBy('name','ASC')
                         ->get();
@@ -31,6 +34,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+        $this->app['request']->server->set('HTTPS', true);
+}
     }
 }

216d6857c12230cceec8f0848a4f5346d4bc6412
M55 <m55@localhost.localdomain>
2017-12-08 09:59:27 +0300

    old views

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -17,6 +17,15 @@ use Illuminate\Support\Facades\Storage;
 
 class AgentController extends Controller
 {
+   public function generateRandomString($length = 10) {
+       $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+       $charactersLength = strlen($characters);
+       $randomString = '';
+       for ($i = 0; $i < $length; $i++) {
+           $randomString .= $characters[rand(0, $charactersLength - 1)];
+       }
+       return $randomString;
+   }
 
   protected function num2str($num) {
   $nul='ноль';
